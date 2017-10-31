0d49823697b69890c794a60ace007cd76d8b4662
Jan Delgado <jdelgado@gmx.net>
2017-10-31 15:13:00 +0100

    -optimized docker image layering -added a default user which is used when a shell in the container is started

--- docker/etc/entrypoint.sh
+++ docker/etc/entrypoint.sh
@@ -5,6 +5,11 @@ chown -R $GOSU_USER /lede
 # If GOSU_USER environment variable set to something other than 0:0 (root:root),
 # become user:group set within and exec command passed in args
 if [ "$GOSU_USER" != "0:0" ]; then
+    # make sure a valid user exists in /etc/passwd
+    if grep "^builder:" /etc/passwd; then
+      sed -i "/^builder:/d" /etc/passwd
+    fi
+    echo "builder:x:$GOSU_USER:LEDE builder:/lede:/bin/bash" >> /etc/passwd
     exec gosu $GOSU_USER "$@"
 fi
 
