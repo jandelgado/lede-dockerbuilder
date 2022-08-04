e2c2f4652064b6182ba1ef96980dcb80e2faf67c
Jan Delgado <jdelgado@gmx.net>
2022-08-04 20:47:54 +0200

    add tests

---
+++ .test/run_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# run all tests for a given configuration. tests are run after images are
+# built. we do some simple assertions on the generated artifiacts to e.g. make
+# sure the configured packages are included etc.
+set -eou pipefail
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CONF=$1
+OUT=$2
+
+echo "----------------------------------------------------------------------"
+echo "run tests for $CONF"
+echo "----------------------------------------------------------------------"
+for t in "$SCRIPT_DIR"/test_*.sh; do 
+    if "$t" "$CONF" "$OUT"; then
+        echo "test OK"
+    else
+        echo "test FAILED"
+    fi
+done
+
+
---
+++ .test/test_manifest_contains_packages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source $1
+DIR=$2  # e.g. output/
+FAIL=0
+
+fail() {
+    echo "  ERROR: $*"
+    FAIL=1
+}
+
+PREFIX="openwrt-$LEDE_RELEASE-$LEDE_TARGET-$LEDE_SUBTARGET-$LEDE_PROFILE"
+MANIFEST="$DIR/$PREFIX.manifest"
+
+echo "Test all configured packages are in manifest file $MANIFEST..."
+if [ ! -f "$MANIFEST" ]; then
+    fail "$MANIFEST does not exist"
+    exit 1
+fi
+
+for p in $LEDE_PACKAGES; do 
+    grep -q -E "^$p" "$MANIFEST" || fail "pkg not in manifest: $p"
+done
+
+exit $FAIL
