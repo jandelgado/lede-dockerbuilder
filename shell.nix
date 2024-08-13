ceb9336b9c8f4fe6d8737df1a6b58b60cad56fda
Jan Delgado <jdelgado@gmx.net>
2024-08-13 13:33:14 +0200

    pin nix version

--- shell.nix
+++ shell.nix
@@ -1,29 +1,34 @@
-with (import <nixpkgs> {});
-mkShell {
+let
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/42c5e250a8a9162c3e962c78a4c393c5ac369093.tar.gz";
+  }) {};
+
+in pkgs.mkShell {
+
   name = "OpenWRT builder";
   buildInputs = [
       pkgs.cacert
-      git
-      perl
-      gnumake
-      gcc
-      unzip
-      utillinux
-      python3
-      rsync
-      patch
-      wget
-      file
-      subversion
-      which
-      pkg-config
-      openssl
-      systemd
-      binutils
+      pkgs.git
+      pkgs.perl
+      pkgs.gnumake
+      pkgs.gcc
+      pkgs.unzip
+      pkgs.utillinux
+      pkgs.python3
+      pkgs.rsync
+      pkgs.patch
+      pkgs.wget
+      pkgs.file
+      pkgs.subversion
+      pkgs.which
+      pkgs.pkg-config
+      pkgs.openssl
+      pkgs.systemd
+      pkgs.binutils
 
-      ncurses
-      zlib
-      zlib.static
-      glibc.static
+      pkgs.ncurses
+      pkgs.zlib
+      pkgs.zlib.static
+      pkgs.glibc.static
   ];
 }
